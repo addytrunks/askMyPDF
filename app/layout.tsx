ae4c2bda895cda94f91dd14eed6b32be84fa1c54
Adhithya Srivatsan <addytrunks@gmail.com>
2024-07-29 20:18:17 +0530

    feat: header section

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,14 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body
+          className={cn(
+            inter.className,
+            "min-h-screen h-screen overflow-hidden flex flex-col"
+          )}
+        >
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
