d9cc5ab724607e3dbe19ad517075933c9c566826
Adhithya Srivatsan <addytrunks@gmail.com>
2024-07-30 21:02:13 +0530

    feat: header

---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { FilePlus } from "lucide-react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between bg-white shadow-md p-4 border-b">
+      <Link href="/dashboard" className="text-2xl font-semibold">
+        AskMy<span className="text-indigo-600">PDF</span>
+      </Link>
+
+      <SignedIn>
+        <div className="flex items-center space-x-2">
+          <Button asChild variant="link" className="hidden md:flex">
+            <Link href="/dashboard/upgrade">Pricing</Link>
+          </Button>
+
+          <Button asChild variant="outline">
+            <Link href="/dashboard/">My Documents</Link>
+          </Button>
+
+          <Button asChild variant="outline" className="border-indigo-600">
+            <Link href="/dashboard/upgrade">
+              <FilePlus className="text-indigo-600" />
+            </Link>
+          </Button>
+
+          <UserButton />
+        </div>
+      </SignedIn>
+    </div>
+  );
+};
+
+export default Header;
