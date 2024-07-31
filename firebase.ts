ec45c78dfe558ccec2be4a314ceaf2b2d82e7f5b
Adhithya Srivatsan <addytrunks@gmail.com>
2024-07-31 20:12:17 +0530

    feat: firebase setup (client+admin)

---
+++ firebase.ts
@@ -0,0 +1,20 @@
+import { getApps, initializeApp, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcYMxNLTRneFRBKS2__eAxLmgEJ-y_Erg",
+  authDomain: "askmypdf-b3b09.firebaseapp.com",
+  projectId: "askmypdf-b3b09",
+  storageBucket: "askmypdf-b3b09.appspot.com",
+  messagingSenderId: "385100958095",
+  appId: "1:385100958095:web:a3d4560f6a5c69864563fa",
+  measurementId: "G-Y7NL1MWKHK",
+};
+
+// Initialize Firebase app if not already initialized
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+export const db = getFirestore(app);
+
+export const storage = getStorage(app);
