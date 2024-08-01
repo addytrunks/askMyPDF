44bc8ad0b33a60b836beb102ed09077f9db2634e
Adhithya Srivatsan <addytrunks@gmail.com>
2024-08-01 15:02:29 +0530

    feat: firestore document upload

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { useToast } from "@/components/ui/use-toast";
 import { FileRejection, useDropzone } from "react-dropzone";
 import {
@@ -11,11 +11,38 @@ import {
   SaveIcon,
 } from "lucide-react";
 import { MAX_FILE_SIZE } from "@/constants";
+import useUpload, { StatusText } from "@/hooks/useUpload";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const FileUploader = () => {
   const { toast } = useToast();
+  const router = useRouter();
+
+  const statusIcons: {
+    [key in StatusText]: JSX.Element;
+  } = {
+    [StatusText.UPLOADING]: (
+      <RocketIcon className="h-20 w-20 text-indigo-600" />
+    ),
+    [StatusText.UPLOADED]: (
+      <CheckCircleIcon className="h-20 w-20 text-indigo-600" />
+    ),
+    [StatusText.SAVING]: <SaveIcon className="h-20 w-20 text-indigo-600" />,
+    [StatusText.GENERATING]: (
+      <HammerIcon className="h-20 w-20 text-indigo-600 animate-bounce" />
+    ),
+  };
+
+  const { progress, status, fileId, handleUpload } = useUpload();
+  useEffect(() => {
+    if (fileId) {
+      router.push(`/dashboard/files/${fileId}`);
+    }
+  }, [fileId, router]);
+
   const onDrop = useCallback(
-    (acceptedFiles: File[], fileRejections: FileRejection[]) => {
+    async (acceptedFiles: File[], fileRejections: FileRejection[]) => {
       fileRejections.forEach(({ file, errors }) => {
         errors.forEach(({ code }) => {
           if (code === "file-too-large") {
@@ -30,17 +57,21 @@ const FileUploader = () => {
             toast({
               title: "Invalid file type",
               variant: "destructive",
-              description: 'The file is not a PDF. Please upload a PDF file',
+              description: "The file is not a PDF. Please upload a PDF file",
             });
           }
         });
       });
 
       if (acceptedFiles.length === 1) {
-        console.log("File uploaded successfully");
+        const file = acceptedFiles[0];
+
+        if (file) {
+          await handleUpload(file);
+        }
       }
     },
-    [toast]
+    [toast, handleUpload]
   );
 
   const { getRootProps, getInputProps, isDragActive, isFocused, isDragAccept } =
@@ -52,31 +83,61 @@ const FileUploader = () => {
       maxFiles: 1,
       maxSize: MAX_FILE_SIZE,
     });
+
+  const uploadInProgress = progress != null && progress >= 0 && progress <= 100;
   return (
     <div className="flex flex-col gap-4 items-center max-w-7xl mx-auto">
-      <div
-        {...getRootProps()}
-        className={`p-10 border-2 border-dashed mt-10 w-[90%]  border-indigo-600 text-indigo-600 rounded-lg h-96 flex items-center justify-center hover:cursor-pointer ${
-          isFocused || isDragAccept || isDragActive
-            ? "bg-indigo-300"
-            : "bg-indigo-100"
-        }`}
-      >
-        <input {...getInputProps()} />
-        <div className="flex flex-col items-center justify-center">
-          {isDragActive ? (
-            <>
-              <RocketIcon className="h-20 w-20 animate-ping" />
-              <p>Drop the files here ...</p>
-            </>
-          ) : (
-            <>
-              <CircleArrowDown className="h-20 w-20 animate-bounce" />
-              <p>Drag & drop some files here, or click to select files</p>
-            </>
-          )}
+      {uploadInProgress && (
+        <div className="mt-32 flex flex-col justify-center items-center gap-5">
+          <div
+            className={cn(
+              "radial-progress bg-indigo-300 text-white border-indigo-600 border-4",
+              progress === 100 && "hidden"
+            )}
+            role="progressbar"
+            style={{
+              // @ts-ignore
+              "--value": progress,
+              "--size": "12rem",
+              thickness: "1.3rem",
+            }}
+          >
+            {progress} %
+          </div>
+
+          {
+            // @ts-ignore
+            statusIcons[status]
+          }
+          {/* @ts-ignore */}
+          <p className="text-indigo-600 animate-pulse">{status}</p>
         </div>
-      </div>
+      )}
+      {!uploadInProgress && (
+        <div
+          {...getRootProps()}
+          className={`p-10 border-2 border-dashed mt-10 w-[90%]  border-indigo-600 text-indigo-600 rounded-lg h-96 flex items-center justify-center hover:cursor-pointer ${
+            isFocused || isDragAccept || isDragActive
+              ? "bg-indigo-300"
+              : "bg-indigo-100"
+          }`}
+        >
+          <input {...getInputProps()} />
+          <div className="flex flex-col items-center justify-center">
+            {isDragActive ? (
+              <>
+                <RocketIcon className="h-20 w-20 animate-ping" />
+                <p>Drop the files here ...</p>
+              </>
+            ) : (
+              <>
+                <CircleArrowDown className="h-20 w-20 animate-bounce" />
+                <p>Drag & drop some files here, or click to select files</p>
+              </>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
---
+++ hooks/useUpload.ts
@@ -0,0 +1,73 @@
+"use client";
+
+import { db, storage } from "@/firebase";
+import { useUser } from "@clerk/nextjs";
+import { doc, setDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+export enum StatusText {
+  UPLOADING = "Uploading file...",
+  UPLOADED = "File uploaded successfully",
+  SAVING = "Saving file to the database...",
+  GENERATING = "Generating AI Embeddings. This will only take a few seconds",
+}
+
+export type Status = StatusText[keyof StatusText];
+
+function useUpload() {
+  const [progress, setProgress] = useState<number | null>(null);
+  const [fileId, setFileId] = useState<string | null>(null);
+  const [status, setStatus] = useState<Status | null>(null);
+  const { user } = useUser();
+
+  const router = useRouter();
+
+  const handleUpload = async (file: File) => {
+    if (!file || !user) return;
+
+    const fileIdToUploadTo = uuidv4();
+
+    const storageRef = ref(storage, `users/${user.id}/${fileIdToUploadTo}`);
+
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const percent = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+        setStatus(StatusText.UPLOADING);
+        setProgress(percent);
+      },
+      (error) => {
+        console.error("Error uploading the file", error);
+      },
+      async () => {
+        setStatus(StatusText.UPLOADED);
+
+        const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+
+        setStatus(StatusText.SAVING);
+        await setDoc(doc(db, "users", user.id, "files", fileIdToUploadTo), {
+          name: file.name,
+          downloadURL: downloadURL,
+          createdAt: new Date(),
+          size: file.size,
+          type: file.type,
+          ref: uploadTask.snapshot.ref.fullPath,
+        });
+
+        setStatus(StatusText.GENERATING);
+
+        setFileId(fileIdToUploadTo);
+      }
+    );
+  };
+  return { progress, status, fileId, handleUpload };
+}
+
+export default useUpload;
