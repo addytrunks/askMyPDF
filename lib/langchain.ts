83579f6e0dab78a00793a6f69a16fd60efd316ed
Adhithya Srivatsan <addytrunks@gmail.com>
2024-08-03 09:31:41 +0530

    feat: refactor code

--- lib/langchain.ts
+++ lib/langchain.ts
@@ -1,68 +1,13 @@
 import { auth } from "@clerk/nextjs/server";
 import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
 import { pineconeClient } from "./pinecone";
-import { Index, RecordMetadata } from "@pinecone-database/pinecone";
 import { PineconeStore } from "@langchain/pinecone";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { adminDb } from "@/firebaseAdmin";
-import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { generateDocs, namespaceExists } from "./utils";
 
 const model = new ChatOpenAI({
   apiKey: process.env.OPENAI_API_KEY,
   model: "gpt-4o-mini",
 });
-
-async function namespaceExists(
-  index: Index<RecordMetadata>,
-  namespace: string
-) {
-  if (namespace === null) throw new Error("No namespace provided");
-
-  const { namespaces } = await index.describeIndexStats();
-  return namespaces?.[namespace] !== undefined;
-}
-
-async function generateDocs(docId: string) {
-  const { userId } = auth();
-
-  if (!userId) {
-    throw new Error("User not authenticated");
-  }
-
-  console.log("--- Fetching the download URL from firestore ---");
-
-  const firebaseRef = await adminDb
-    .collection("users")
-    .doc(userId)
-    .collection("files")
-    .doc(docId)
-    .get();
-
-  const downloadURL = firebaseRef.data()?.downloadURL;
-
-  if (!downloadURL) {
-    throw new Error("Download URL not found");
-  }
-
-  console.log("--- Fetched download URL successfully ---");
-
-  // Download the file from the URL
-  const response = await fetch(downloadURL);
-
-  const data = await response.blob();
-
-  console.log("--- Loading PDF file ---");
-  const loader = new PDFLoader(data);
-  const docs = await loader.load();
-
-  console.log("--- Splitting the document into smaller parts/chunks ---");
-  const splitter = new RecursiveCharacterTextSplitter();
-
-  const splitDocs = await splitter.splitDocuments(docs);
-  console.log(`--- Split into ${splitDocs.length} parts ---`);
-
-  return splitDocs;
-}
 
 export const indexName = "addy";
 
