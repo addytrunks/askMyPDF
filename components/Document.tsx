ec9a837f08b65e842886a662c914c28225f59610
Adhithya Srivatsan <addytrunks@gmail.com>
2024-08-05 22:07:50 +0530

    feat: display documents & download

---
+++ components/Document.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import byteSize from "byte-size";
+import { Button } from "./ui/button";
+import { DownloadCloud, Trash2Icon } from "lucide-react";
+import Link from "next/link";
+
+const Document = ({
+  id,
+  name,
+  size,
+  downloadUrl,
+}: {
+  id: string;
+  name: string;
+  size: number;
+  downloadUrl:string;
+}) => {
+  const router = useRouter();
+  return (
+    <div className="flex flex-col w-64 h-80 rounded-xl bg-white drop-shadow-md justify-between p-4 transition-all transform hover:scale-105 hover:bg-indigo-600 hover:text-white cursor-pointer group">
+      <div
+        className="flex-1"
+        onClick={() => {
+          router.push(`/dashboard/files/${id}`);
+        }}
+      >
+        <p className="font-semibold line-clamp-2">{name}</p>
+        <p className="text-sm text-gray-500 group-hover:text-indigo-100">
+          {byteSize(size).value} KB
+        </p>
+      </div>
+
+      <div className="flex space-x-2 justify-end">
+        {/* <Button
+          variant="outline"
+          disabled={isDeleting || !hasActiveMembership}
+          onClick={() => {
+            const prompt = window.confirm(
+              "Are you sure you want to delete this document?"
+            );
+
+            if (prompt) {
+              // delete document
+              startTransaction(async () => {
+                await deleteDocument(id);
+              });
+            }
+          }}
+        >
+          <Trash2Icon className="h-6 w-6 text-red-500" />
+
+        </Button> */}
+
+        <Button variant="outline" asChild>
+          <a href={downloadUrl}>
+            <DownloadCloud className="h-6 w-6 text-indigo-600" />
+          </a>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Document;
--- components/Documents.tsx
+++ components/Documents.tsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import PlaceholderDocument from './PlaceholderDocument'
+import React from "react";
+import PlaceholderDocument from "./PlaceholderDocument";
+import { auth } from "@clerk/nextjs/server";
+import { adminDb } from "@/firebaseAdmin";
+import Document from '@/components/Document'
 
-const Documents = () => {
+const Documents = async () => {
+  auth().protect();
+
+  const { userId } = auth();
+
+  if (!userId) {
+    throw new Error("User not found");
+  }
+
+  const documentsSnapshot = await adminDb
+    .collection("users")
+    .doc(userId)
+    .collection("files")
+    .get();
   return (
-    <div className='flex flex-wrap p-5 bg-gray-50 justify-center lg:justify-start rounded-sm gap-5 max-w-7xl mx-auto'>
-        <PlaceholderDocument/>
-    </div>
-  )
-}
+    <div className="flex flex-wrap p-5 bg-gray-50 justify-center lg:justify-start rounded-sm gap-5 max-w-7xl mx-auto">
+      {documentsSnapshot.docs.map((doc) => {
+        const { name, downloadURL:downloadUrl, size } = doc.data();
+        console.log(doc.data());
 
-export default Documents
+        return (
+          <Document
+            key={doc.id}
+            id={doc.id}
+            name={name}
+            size={size}
+            downloadUrl={downloadUrl}
+          />
+        );
+      })}
+      <PlaceholderDocument />
+    </div>
+  );
+};
+
+export default Documents;
